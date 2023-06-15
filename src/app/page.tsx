a9c4b1b209c07151d7b8bfd4b141d3d6403a85ce
yoshidaman99 <50310497+yoshidaman99@users.noreply.github.com>
2023-06-15 16:16:02 +0800

    fix the throw to the main page for login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between px-24">
       
         <div className='w-full'>
           <Image
