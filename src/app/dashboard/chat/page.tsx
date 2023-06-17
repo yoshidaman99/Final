900601fa07de6e7d4ccbd81ac8c511c0c9cffc4a
yoshidaman99 <50310497+yoshidaman99@users.noreply.github.com>
2023-06-17 12:41:27 +0800

    NO error on the chat admin

--- src/app/dashboard/chat/page.tsx
+++ src/app/dashboard/chat/page.tsx
@@ -1,13 +1,12 @@
 'use client'
-'use client'
 import { useEffect } from "react";
-import  { StaticRouter }  from "react-router-dom/server";
-import { AuthProvider } from '@/app/context/AuthContext';
+import { AuthProvider } from "@/app/context/AuthContext";
 import ChatBox from "@/app/components/ChatBox_2";
 import SendMessage from "@/app/components/UI/SendMessage_2";
-import HeaderInfo from '@/app/components/header_info';
+import HeaderInfo from "@/app/components/header_info";
+import { BrowserRouter } from "react-router-dom";
 
-const Chat = (req : any ) => {
+const Chat = (req: any) => {
   useEffect(() => {
     if (typeof document !== "undefined") {
       document.documentElement.scrollTop = 0;
@@ -17,20 +16,24 @@ const Chat = (req : any ) => {
   return (
     <section className="containerWrap">
       <div className="fixed w-full text-right">
-        <HeaderInfo title={'Admin Chat'} />
+        <HeaderInfo title={"Admin Chat"} />
       </div>
       <div>
-        <h1 className='pt-14 text-3xl font-bold text-center underline'>MessageBoard</h1>
-        <StaticRouter location={req.url}>
-          <AuthProvider>
-            <div className="flex-1 scroll-mt-350 px-8">
-              <ChatBox />
-            </div>
-            <div className="containerWrap">
-              <SendMessage />
-            </div>
-          </AuthProvider>
-        </StaticRouter>
+        <h1 className="pt-14 text-3xl font-bold text-center underline">
+          MessageBoard
+        </h1>
+        {typeof document !== "undefined" && (
+          <BrowserRouter>
+            <AuthProvider>
+              <div className="flex-1 scroll-mt-350 px-8">
+                <ChatBox />
+              </div>
+              <div className="containerWrap">
+                <SendMessage />
+              </div>
+            </AuthProvider>
+          </BrowserRouter>
+        )}
       </div>
     </section>
   );
