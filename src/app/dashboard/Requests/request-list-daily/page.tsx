8e1c85def88fed90c9256ecf7c20704c429ec791
yoshidaman99 <50310497+yoshidaman99@users.noreply.github.com>
2023-06-23 06:19:25 +0800

    final

--- src/app/dashboard/Requests/request-list-daily/page.tsx
+++ src/app/dashboard/Requests/request-list-daily/page.tsx
@@ -1,19 +1,14 @@
 'use client'
 import React from 'react';
-import { Metadata } from 'next';
 import HeaderInfo from '@/app/components/header_info';
 import { RequestList } from '@/lib/getRequestListPerDay';
-
-export const metadata: Metadata = {
-    title: 'Request List',
-};
 
 export default function requestList() {
 
     return (
         <section>
         <div>
-            <HeaderInfo title={metadata.title != null ? metadata.title.toString() : ''} />
+            <HeaderInfo title={'Request List'} />
         </div>
         <div className='p-4'>
             <div className='p-3 bg-slate-100 rounded'>
